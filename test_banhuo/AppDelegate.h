f9682b2ab86f9b0aeb8e10c972b78bb772146856
hechaohe <550121308@qq.com>
2017-01-20 09:10:35 +0800

    Initial Commit

---
+++ test_banhuo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_banhuo
+//
+//  Created by hc on 2017/1/20.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_banhuo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_banhuo
+//
+//  Created by hc on 2017/1/20.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
